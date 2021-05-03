02ad64c78a7617ba34a5d0ad4a6ffbe806ccf20d
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-05-02 23:29:56 -0500

    Constantes

---
+++ Fundamentos/constantes.py
@@ -0,0 +1,13 @@
+# Las constantes no funcionan en python solo se puede hacer una adaptacion para que funcione como simulacion de constante
+
+# Las constantes van en mayuscula
+
+MI_CONSTANTE = "Valor de la constante"
+
+
+class Matematicas:
+    PI = 3.1416
+
+
+# print(MI_CONSTANTE)
+print(Matematicas.PI)
